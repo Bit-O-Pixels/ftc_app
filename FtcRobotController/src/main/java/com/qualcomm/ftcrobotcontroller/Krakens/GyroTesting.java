36a94590f6d1f38d7bf100c52d2a521324f3ebb5
Loki <lokio.casebstv@gmail.com>
2015-11-11 17:28:29 -0600

    KrakensStuff
    Added the krakensstuff

---
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/Krakens/GyroTesting.java
@@ -0,0 +1,15 @@
+package com.qualcomm.ftcrobotcontroller.Krakens;
+
+public class GyroTesting extends Krakentelementry
+
+{
+
+        public GyroTesting() {}
+
+        @Override public void loop () {
+
+
+
+
+        }
+}
